ba32f7cec9ac676fcee3410146fadc336848302c
Liam <liamdidato@gmail.com>
2022-01-29 16:38:56 +1000

    Added some ugly level select stuff to start with

---
+++ Assets/Scripts/LevelSelectController.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelSelectController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+        if (Physics.Raycast(ray, out hit))
+        {
+            if (Input.GetKeyDown(KeyCode.Mouse0))
+            {
+                hit.collider.SendMessageUpwards("HandleClick");
+            } else
+            {
+                hit.collider.SendMessageUpwards("HandleHover");
+            }
+        }
+    }
+}
