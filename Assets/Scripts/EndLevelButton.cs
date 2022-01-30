3de64f7e1a97916c93aa0ae07286bcb800d20546
Liam <liamdidato@gmail.com>
2022-01-30 14:44:02 +1000

    Fade out when going back to menu

--- Assets/Scripts/EndLevelButton.cs
+++ Assets/Scripts/EndLevelButton.cs
@@ -4,18 +4,29 @@ using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class EndLevelButton : MonoBehaviour
 {
+    private readonly float FADE_DURATION = 1.0f;
+    private readonly Color TRANSPARENT_BLACK = new Color(0, 0, 0, 0);
+
     private TextMeshPro textMesh;
     private AudioSource audioSource;
+    private AudioSource bgMusicAudioSource;
     private Color initialColor;
+    private bool isFading;
+    private float fadeProgress;
     public GameObject Text;
+    public GameObject BGMusicGameObject;
+    public AudioClip OnExitSound;
+    public Image BlackImage;
 
     // Start is called before the first frame update
     void Start()
     {
         audioSource = GetComponent<AudioSource>();
+        bgMusicAudioSource = BGMusicGameObject.GetComponent<AudioSource>();
         textMesh = Text.GetComponent<TextMeshPro>();
         initialColor = textMesh.color;
     }
@@ -23,7 +34,7 @@ public class EndLevelButton : MonoBehaviour
     private void OnMouseEnter()
     {
         textMesh.color = Color.white;
-        if (GlobalGameState.GameState.SoundEnabled)
+        if (GlobalGameState.GameState.SoundEnabled && !isFading)
         {
             audioSource.Play();
         }
@@ -36,12 +47,27 @@ public class EndLevelButton : MonoBehaviour
 
     private void OnMouseUpAsButton()
     {
-        SceneManager.LoadScene("LevelSelectScene");
+        if (isFading) return;
+        audioSource.PlayOneShot(OnExitSound);
+        isFading = true;
+        fadeProgress = 0f;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (isFading)
+        {
+            fadeProgress += Time.deltaTime;
+            var prog = fadeProgress / FADE_DURATION;
+            var newColor = Color.Lerp(TRANSPARENT_BLACK, Color.black, prog);
+            var newVol = Mathf.Lerp(1, 0, prog);
+            BlackImage.color = newColor;
+            bgMusicAudioSource.volume = newVol;
+            if (prog >= 1f)
+            {
+                SceneManager.LoadScene("LevelSelectScene");
+            }
+        }
     }
 }
