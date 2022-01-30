087527a76c472d86bf057c691ca69b264d589abb
Liam <liamdidato@gmail.com>
2022-01-30 20:38:09 +1000

    Some final credits stuff that I didnt push earlier

--- Assets/Scripts/CreditsButton.cs
+++ Assets/Scripts/CreditsButton.cs
@@ -1,14 +1,16 @@
+using Assets.Scripts;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class CreditsButton : MonoBehaviour
 {
+    private AudioSource audioSource;
     public GameObject SelectionHighlight;
     public GameObject CreditsObject;
     void Start()
     {
-        
+        audioSource = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
@@ -19,6 +21,10 @@ public class CreditsButton : MonoBehaviour
 
     private void OnMouseEnter()
     {
+        if (GlobalGameState.GameState.SoundEnabled)
+        {
+            audioSource.Play();
+        }
         SelectionHighlight.SetActive(true);
     }
 
--- Assets/Scripts/CreditsController.cs
+++ Assets/Scripts/CreditsController.cs
@@ -1,13 +1,16 @@
-﻿using System.Collections;
+﻿using Assets.Scripts;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class CreditsController : MonoBehaviour
 {
+    private AudioSource audioSource;
     public GameObject SelectionHighlight;
     void Start()
     {
         SelectionHighlight.SetActive(false);
+        audioSource = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
@@ -18,6 +21,10 @@ public class CreditsController : MonoBehaviour
 
     private void OnMouseEnter()
     {
+        if (GlobalGameState.GameState.SoundEnabled)
+        {
+            audioSource.Play();
+        }
         SelectionHighlight.SetActive(true);
     }
 
